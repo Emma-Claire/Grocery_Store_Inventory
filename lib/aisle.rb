d60253118c26902f8b802c2cfcbba9d0168c3ee3
Emma Claire Philippides <acme9898@gmail.com>
2017-04-01 21:03:55 -0700

    changes to aisle csv as well as updating aisle class and spec with changes

--- lib/aisle.rb
+++ lib/aisle.rb
@@ -2,27 +2,25 @@ require 'csv'
 
 module GroceryStoreSystem
   class Aisle
-    attr_accessor :product_id, :product_name, :aisle_id, :aisle_name, :product_quantity
+    attr_accessor :aisle_id, :aisle_name, :aisle_description
     attr_reader :store_id
 
     def initialize(aisle_info)
       @store_id = aisle_info[:store_id].to_i
-      @product_id = aisle_info[:product_id].to_i
-      @product_name = aisle_info[:product_name].to_s
       @aisle_id = aisle_info[:aisle_id].to_s
       @aisle_name = aisle_info[:aisle_name].to_s
-      @product_quantity = aisle_info[:product_quantity].to_i
+      @aisle_description = aisle_info[:aisle_description].to_s
 
     end
 
-    # def self.all
-    #   #returns all aisles
-    #   @aisles_info = []
-    #   CSV.foreach("support/aisle.csv", {:headers => true}) do |line|
-    #     @aisles_info << self.new({store_id:line[0], product_id:line[1], product_name:line[2], aisle_id:line[3], aisle_name:line[4], product_quantity:line[5]})
-    #   end
-    #   return @aisles_info
-    # end
+    def self.all
+      #returns all aisles
+      @aisles_info = []
+      CSV.foreach("support/aisle.csv", {:headers => true}) do |line|
+        @aisles_info << self.new({store_id:line[0], aisle_id:line[1], aisle_name:line[2], aisle_description:line[3]})
+      end
+      return @aisles_info
+    end
     #
     # def self.find(product_name)
     #   #returns aisle given a product_id
--- specs/aisle_spec.rb
+++ specs/aisle_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe "GroceryStoreSystem::Aisle" do
 
-  let(:new_aisle) {GroceryStoreSystem::Aisle.new({store_id: 12345, product_id: 103, product_name: 'Brownie', aisle_id: 'B', aisle_name: 'Bakery', product_quantity: 123})}
+  let(:new_aisle) {GroceryStoreSystem::Aisle.new({store_id: 12345, aisle_id: 'B', aisle_name: 'Bakery'})}
 
   describe "#initialize" do
     it "must be an instance of Aisle class" do
@@ -11,12 +11,6 @@ describe "GroceryStoreSystem::Aisle" do
     it "takes an integer for store_id" do
       new_aisle.store_id.must_be_kind_of Integer
     end
-    it "takes an interger for product_id" do
-      new_aisle.product_id.must_be_instance_of Integer
-    end
-    it "takes a string for product_name" do
-      new_aisle.product_name.must_be_instance_of String
-    end
 
     it "takes a string for aisle_id" do
       new_aisle.aisle_id.must_be_instance_of String
@@ -24,12 +18,31 @@ describe "GroceryStoreSystem::Aisle" do
     it "takes a string for aisle_name" do
       new_aisle.aisle_name.must_be_instance_of String
     end
-    it "takes an Integer for product_quantity" do
-      new_aisle.product_quantity.must_be_instance_of Integer
+    it "takes a string for aisle_description" do
+      new_aisle.aisle_description.must_be_instance_of String
     end
+  end
+
+  describe "self.all" do
+    it "outputs an array of all aisle and their info" do
+      aisles_array = GroceryStoreSystem::Aisle.all
+      aisles_array.must_be_instance_of Array
+
+      aisles_array.each do |aisle|
+        aisle.class.must_equal GroceryStoreSystem::Aisle
+      end
+
+      aisles_array.length.must_equal(10)
+
+      aisles_array[0].aisle_name.must_equal('Fruits') # checking first element of array
+      aisles_array[0].aisle_id.must_equal('A')
+
+      aisles_array[9].aisle_name.must_equal('Flowers') # checking last element of array
+      aisles_array[9].aisle_id.must_equal('J')
+    end
+  end
+
+  describe "self.find" do
 
   end
-  # it "returns an array with the correct number of products for any given aisle" do
-  #
-  # end
 end
